9c2583b4d4e16683c147bad8e1d9e953eb39b2a8
msquare-Kmutt <montree.cpe@mail.kmutt.ac.th>
2016-05-17 16:27:29 +0700

    Add files via upload
    php service logout

---
+++ Code/Logout.php
@@ -0,0 +1,18 @@
+<?php
+	include("Connect.php");
+
+	$con = mysqli_connect($host, $username, $password, $databaseName);
+	if (mysqli_connect_errno())
+	{
+		echo "false";
+	}
+	else
+	{
+		session_start();
+		if(session_destroy())
+		{
+			header("Location: index.html");
+		}
+	}
+	mysqli_close($con);
+?>
