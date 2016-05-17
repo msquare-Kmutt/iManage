84fd2151f876fd7ef1a12c5d68ee90f44305e4fa
msquare-Kmutt <montree.cpe@mail.kmutt.ac.th>
2016-05-17 16:09:17 +0700

    Add files via upload
    Edit sectionName

---
+++ Code/SectionName.php
@@ -0,0 +1,29 @@
+<?php
+	include("Connect.php");
+
+	$con = mysqli_connect($host, $username, $password, $databaseName);
+	if (mysqli_connect_errno())
+	{
+		echo "false";
+	}
+	else
+	{
+		session_start();
+		$userID = mysqli_real_escape_string($con, $_SESSION['username']);
+		$sql = "SELECT  COUNT(*) AS count, userFirstName FROM userInformation WHERE userID = '" . $userID . "'";
+		$result = mysqli_query($con, $sql);
+		$out = $result->fetch_array(MYSQLI_ASSOC);
+		if($out['count'] == "1")
+		{
+			$rs = '"[{"userName":"' . $out["userFirstName"] . '"}]';
+			echo $rs;
+
+		}
+		else
+		{
+			header("Location: Index.html");
+			echo "false";
+		}
+	}
+	mysqli_close($con);
+?>
